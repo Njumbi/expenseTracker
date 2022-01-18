2861118d08ea096c82543029883284b676a6295b
njumbi <wambuinjumbi@gmail.com>
2022-01-18 09:28:38 +0300

    feat: add query functions

---
+++ app/src/main/java/com/example/expensetracker/data/TransactionDao.kt
@@ -0,0 +1,23 @@
+package com.example.expensetracker.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface TransactionDao {
+
+    @Query("SELECT * FROM `transaction`")
+    fun getAllTransactions(): Flow<List<Transaction>>
+
+    @Insert
+   suspend fun insertTransaction(transaction: Transaction)
+
+    // get all income or expense list by transaction type param
+    @Query("SELECT * FROM `transaction` WHERE `type` == :transactionType")
+    fun getAllSingleTransaction(transactionType: String): Flow<List<Transaction>>
+
+
+}
