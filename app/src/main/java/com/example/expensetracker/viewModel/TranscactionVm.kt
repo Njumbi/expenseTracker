3f69cee37e4235a7cc2652bc79f4c84bff0891e6
Njumbi <wambuinjumbi@gmail.com>
2022-03-23 23:37:20 +0300

    refactor: code clean up

--- app/src/main/java/com/example/expensetracker/viewModel/TranscactionVm.kt
+++ app/src/main/java/com/example/expensetracker/viewModel/TranscactionVm.kt
@@ -1,13 +1,20 @@
 package com.example.expensetracker.viewModel
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.expensetracker.data.Transaction
 import com.example.expensetracker.repository.TransactionRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -19,7 +26,23 @@ enum class TransactionType {
 @HiltViewModel
 class TranscactionVm @Inject constructor(val repository: TransactionRepository) : ViewModel() {
 
-    val transactions: LiveData<List<Transaction>> = repository.fetchAllTransactions().asLiveData()
+    private val _transactions = MutableSharedFlow<Transaction>()
+    val transactions = _transactions.asSharedFlow()
+
+    private val _allTransactions = MutableSharedFlow<List<Transaction>>()
+    val allTransactions = _allTransactions.asSharedFlow()
+
+
+//    val transactions: LiveData<List<Transaction>> = repository.fetchAllTransactions().asLiveData()
+
+    fun fetchAllTransactions() {
+        viewModelScope.launch {
+            val response = repository.fetchAllTransactions()
+            response.collect {
+                _allTransactions.emit(it)
+            }
+        }
+    }
 
     fun addTransaction(transaction: Transaction) {
         viewModelScope.launch {
@@ -27,25 +50,36 @@ class TranscactionVm @Inject constructor(val repository: TransactionRepository) 
         }
     }
 
-    private val totalIncome: Flow<Double> =
-        repository.getAllSingleTransaction(TransactionType.Income.toString())
-            .map {
-                var total = 0.0;
-                it.forEach {
-                    total += it.amount!!
+    val totalIncome: Flow<Double>
+        get() {
+            return repository.getAllSingleTransaction(TransactionType.Income.toString())
+                .map {
+                    var total = 0.0;
+                    it.forEach {
+                        if (it.type == TransactionType.Income.toString()) {
+                            total += it.amount!!
+                        }
+                    }
+                    return@map total
                 }
-                return@map total
-            }
+        }
 
-    private val totalExpenses: Flow<Double> =
-        repository.getAllSingleTransaction(TransactionType.Expenses.toString())
-            .map {
-                var totalExpenses = 0.0;
-                it.forEach {
-                    totalExpenses += it.amount!!
+
+ val totalExpenses: Flow<Double>
+        get() {
+            return repository.getAllSingleTransaction(TransactionType.Expenses.toString())
+                .map {
+                    var totalExpenses = 0.0;
+                    it.forEach {
+                        if (it.type == TransactionType.Expenses.toString()){
+                            totalExpenses += it.amount!!
+                        }
+
+                    }
+                    return@map totalExpenses
                 }
-                return@map totalExpenses
-            }
+        }
+
 
     fun calculateIncome(): StateFlow<Double> =
         flow<Double> {
@@ -75,19 +109,20 @@ class TranscactionVm @Inject constructor(val repository: TransactionRepository) 
             0.0
         )
 
-    val totalValue: StateFlow<Double> = flow {
-        combine(totalExpenses, totalIncome) { a, b ->
-            return@combine ( b-a )
-        }.collect {
-            emit(it)
-        }.runCatching {
-            emit(0.0)
+    val totalValue: StateFlow<Double>
+        get() {
+            return flow {
+                combine(totalExpenses, totalIncome) { a, b ->
+                    return@combine (b - a)
+                }.collect {
+                    emit(it)
+                }.runCatching {
+                    emit(0.0)
+                }
+            }.stateIn(
+                viewModelScope,
+                SharingStarted.Lazily,
+                0.0
+            )
         }
-    }.stateIn(
-        viewModelScope,
-        SharingStarted.Lazily,
-        0.0
-    )
-
-
 }
