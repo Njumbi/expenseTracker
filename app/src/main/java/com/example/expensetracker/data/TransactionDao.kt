0a2971907c332b86f80c7ce5734e3930d6aa8860
Njumbi <wambuinjumbi@gmail.com>
2022-03-23 23:40:18 +0300

    feat: add delete method

--- app/src/main/java/com/example/expensetracker/data/TransactionDao.kt
+++ app/src/main/java/com/example/expensetracker/data/TransactionDao.kt
@@ -1,6 +1,7 @@
 package com.example.expensetracker.data
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
@@ -18,6 +19,10 @@ interface TransactionDao {
     // get all income or expense list by transaction type param
     @Query("SELECT * FROM `transaction` WHERE `type` == :transactionType")
     fun getAllSingleTransaction(transactionType: String): Flow<List<Transaction>>
+
+    @Delete()
+    fun nuke(transaction: Transaction)
+
 
 
 }
